e244270c5b52399bf9d9f3003ce5a6c984115f8e
David Adeniyi <dta123adeniyi@gmail.com>
2024-09-24 23:08:29 -0400

    Begun working on front-end

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/",methods=["GET"])
 def index():
-    return render_template('index.html')
+    grid = []
+    for i in range(10):
+        inner_grid = []
+        for j in range(10):
+            inner_grid.append([i, j])
+        grid.append(inner_grid)
+
+    return render_template('index.html', grid=grid)
 
 if __name__ == "__main__":
     app.run(debug=True)
